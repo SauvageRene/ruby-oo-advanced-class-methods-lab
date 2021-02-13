34c2ddffa82e7b83d57cee321f7542d8bf2b6f59
Rene <renegarcia1864@gmail.com>
2021-02-12 18:15:50 -0600

    commit tree cd1a58bbb1bcf5dbdde536ad7e1d70415a846a54

--- lib/song.rb
+++ lib/song.rb
@@ -9,54 +9,62 @@ class Song
   def save
     self.class.all << self
   end
+
   def self.create
     song = self.new
     song.save
     song
   end
+
   def self.new_by_name(name)
     song = self.new
     song.name = name
     song
   end
+
   def self.create_by_name(name)
     song = self.new
-    song.name = name 
+    song.name = name
     song.save
     song
   end
+
   def self.find_by_name(name)
-    @@all.detect{|song| song.name == name}
+    self.all.find{|song| song.name == name}
   end
+
   def self.find_or_create_by_name(name)
-  #   if self.find_by_name(name)
-  #     self.find_by_name(name)
-  #   else
-  #     self.create_by_name(name)
-  #   end
-  self.find_by_name(name) || self.create_by_name(name)
-  end
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+    self.find_by_name(name) || self.create_by_name(name)
+  end #Returning the song object using "OR"
+
   def self.alphabetical
-    @@all.sort_by{|song| song.name}
+    self.all.sort_by{|song| song.name}
   end
+
   def self.new_from_filename(filename)
+    split_filename = filename.chomp(".mp3").split(" - ") #in this logic we delete the .mp3 at the end of the song and we split (" - ") to have an array of ["Taylor Swift", "Blank Space"]
+    song = self.new
+    song.name = split_filename[1] #assigned name to 'Blank Space'
+    song.artist_name = split_filename[0] #assinging artist to "Taylor Swift"
+    song
+  end
+
+  def self.create_from_filename(filename)
     split_filename = filename.chomp(".mp3").split(" - ")
     song = self.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
-    song
-  end
-def self.create_from_filename(filename)
-  split_filename = filename.chomp(".mp3").split(" - ")
-    song = self.new
-    song.name = split_filename[1]
-    song.artist_name = split_filename[0]
     song.save
     song
-end 
-def self.destroy_all
-  @@all.clear
-  #self.all.clear ; also works 
-end
+  end
 
-end
+  def self.destroy_all
+    @@all.clear
+  end
+
+end 
